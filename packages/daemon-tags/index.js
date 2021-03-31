3e14c3fbe746c40ff473967073dea4430a52ac10
Andric LibreSinn <andric.droid@gmail.com>
2021-03-30 23:03:20 -0400

    Renaming packages

---
+++ packages/daemon-tags/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const move = require('./lib/move')
+
+module.exports = {
+  // Used primarily for testing. Or if you want to inject a function to do your reading for you.
+  unmanaged: {
+    move,
+  },
+  managed: {
+    move: move(fs),
+  }
+}
