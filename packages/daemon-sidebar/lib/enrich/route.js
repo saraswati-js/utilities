4839d15c3d4804cc5ed9328dff96797d130d7547
Andric LibreSinn <andric.droid@gmail.com>
2021-03-31 11:02:46 -0400

    Removing project from sidebar generation

--- packages/daemon-sidebar/lib/enrich/route.js
+++ packages/daemon-sidebar/lib/enrich/route.js
@@ -2,14 +2,8 @@
 module.exports = (frontmatter) => {
   const lastPathPart = (frontmatter.route || frontmatter.slug).replace(/\//g, '')
 
-  // console.log({
-  //   route: [frontmatter.project, frontmatter.basepath, lastPathPart].join('/'),
-  // })
-
-  const route = [frontmatter.project, frontmatter.basepath, lastPathPart].join('/')
-
   return {
-    href: [frontmatter.project, frontmatter.basepath, lastPathPart].join('/'),
+    href: [frontmatter.basepath, lastPathPart].join('/'),
     ...frontmatter,
   }
 }
