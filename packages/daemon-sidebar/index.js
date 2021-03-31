d27fda3f12adeeaf2ee9d468216259a4a39049ca
Andric LibreSinn <andric.droid@gmail.com>
2021-03-30 23:09:58 -0400

    Set up

--- packages/daemon-sidebar/index.js
+++ packages/daemon-sidebar/index.js
@@ -1,6 +1,30 @@
-// write.test(sidebar(db))
+#!/usr/bin/env node
 
-module.exports = {
-  sidebar,
-  saraswatiDb,
+console.clear()
+
+const reduce = (acc, curr) => {
+  const [head, ...tail] = curr
+
+  return {
+    ...acc,
+    [head.replace('--', '')]: tail[0],
+  }
 }
+
+const args = process.argv.slice(2)
+  .map(arg => arg.split('='))
+  .reduce(reduce, {})
+
+if (!args.to || !args.from) {
+  console.log('@saraswati-js:sidebar --from=_fixtures/_posts/ --to=./_fixtures/sidebar.json')
+  process.exit('')
+}
+
+const fs = require('fs')
+const { sidebarLib } = require('../lib')
+const saraswatiDb = sidebarLib.db
+const db = saraswatiDb(args.from)
+const sidebar = sidebarLib.sidebar
+const data = sidebar(db)
+
+fs.writeFileSync(args.to, JSON.stringify(data, null, 2), 'utf8')
